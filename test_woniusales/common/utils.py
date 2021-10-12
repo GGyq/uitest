bf88f830d878cb0358be65659e065003d89f2f19
GYQ <873141952@qq.com>
2021-10-12 14:32:03 +0800

    因test数据库重建导致测试报告页调整

--- test_woniusales/common/utils.py
+++ test_woniusales/common/utils.py
@@ -13,7 +13,7 @@ driver_path = os.path.join(base_path, 'driver')
 img_path = os.path.join(base_path, 'img')
 page_path = os.path.join(base_path, 'page')
 report_path = os.path.join(base_path, 'report')
-cd = Con_DB('root', '123456', 'testresult')
+cd = Con_DB('root', '123456', 'test')
 cd_cont = cd.con_db()
 
 
@@ -70,29 +70,30 @@ def open_browser(browser, url):
     driver.implicitly_wait(30)
     return driver
 
+
 # 获取报告
 def get_result(version):
-    sql=f'select * from test_result where version="{version}"'
-    res=cd.query_all(sql)
-    total=len(res)
-    summery_all={'Pass':0,'Fail':0,'Skip':0}   # 通过数，失败数，跳过数
-    summery_module={}   # 每个模块用例数，通过数，失败数，跳过数
+    sql = f'select * from test_result where version="{version}"'
+    res = cd.query_all(sql)
+    total = len(res)
+    summery_all = {'Pass': 0, 'Fail': 0, 'Skip': 0}  # 通过数，失败数，跳过数
+    summery_module = {}  # 每个模块用例数，通过数，失败数，跳过数
     for i in res:
-        summery_all[i[4]]+=1
-        if i[1] in summery_module:
-            summery_module[i[1]]['Total']+=1
+        summery_all[i[3]] += 1
+        if i[0] in summery_module:
+            summery_module[i[0]]['Total'] += 1
         else:
-            summery_module[i[1]]={}
-            summery_module[i[1]]['Total']=1
-            summery_module[i[1]].update({'Pass':0,'Fail':0,'Skip':0})  # 字典的update方法，把另一个字典添加到这个字典中
+            summery_module[i[0]] = {}
+            summery_module[i[0]]['Total'] = 1
+            summery_module[i[0]].update({'Pass': 0, 'Fail': 0, 'Skip': 0})  # 字典的update方法，把另一个字典添加到这个字典中
 
-        summery_module[i[1]][i[4]]+=1
+        summery_module[i[0]][i[3]] += 1
 
-    return total,summery_all,summery_module
+    return total, summery_all, summery_module
 
 
 # 编辑测试报告页
-def summery_report(v,t,total,summery_all,summery_module,path):
+def summery_report(v, t, total, summery_all, summery_module, path):
     '''
     生成报告，用例统计，分模块的用例统计
     :param v: 版本号
@@ -103,23 +104,23 @@ def summery_report(v,t,total,summery_all,summery_module,path):
     :param path: 保存报告路径
     :return:
     '''
-    file=report_path+'/result_demo.html'
-    s=None
-    with open(file,'r',encoding='utf8') as f:
-        s=f.read()
-        s=s.replace('&version',v)    # 版本号
-        s=s.replace('&data',get_time())  # 测试时间
-        s=s.replace('&tester',t)  # 测试人员
-        s=s.replace('&total',str(total))  # 用例总数
-        s=s.replace('&pass',str(summery_all['Pass']))  # 通过数
-        s=s.replace('&fail', str(summery_all['Fail']))   # 失败数
-        s=s.replace('&skip', str(summery_all['Skip']))   # 跳过数
-        r=f"{100*summery_all['Pass']/total:.2f}%"    # 通过率
-        s=s.replace('&rate',r)     # 模块的统计
-        detail=''
-        n=1
+    file = report_path + '/result_demo.html'
+    s = None
+    with open(file, 'r', encoding='utf8') as f:
+        s = f.read()
+        s = s.replace('&version', v)  # 版本号
+        s = s.replace('&data', get_time())  # 测试时间
+        s = s.replace('&tester', t)  # 测试人员
+        s = s.replace('&total', str(total))  # 用例总数
+        s = s.replace('&pass', str(summery_all['Pass']))  # 通过数
+        s = s.replace('&fail', str(summery_all['Fail']))  # 失败数
+        s = s.replace('&skip', str(summery_all['Skip']))  # 跳过数
+        r = f"{100 * summery_all['Pass'] / total:.2f}%"  # 通过率
+        s = s.replace('&rate', r)  # 模块的统计
+        detail = ''
+        n = 1
         for i in summery_module:
-            tmp=f'''
+            tmp = f'''
             <tr>
                 <td class="bottom" width="100">{n}</td>
                 <td class="bottom">{i}</td>
@@ -131,16 +132,15 @@ def summery_report(v,t,total,summery_all,summery_module,path):
                 <td class="bottom">{summery_module[i]['Skip']}</td>
             </tr>
             '''
-            n+=1
-            detail+=tmp
-        s=s.replace('&detail',detail)
+            n += 1
+            detail += tmp
+        s = s.replace('&detail', detail)
 
-    with open(path+'/summery.html','w',encoding='utf8') as f:
+    with open(path + '/summery.html', 'w', encoding='utf8') as f:
         f.write(s)
 
 
-
-def module_report(v,module,summery_module,path):
+def module_report(v, module, summery_module, path):
     '''
     生成指定模块的报告
     :param v:     # 版本号
@@ -151,7 +151,7 @@ def module_report(v,module,summery_module,path):
     '''
     file = report_path + '/module_demo.html'
     s = None
-    module_info=summery_module[module]  # 获取模块的信息
+    module_info = summery_module[module]  # 获取模块的信息
     with open(file, 'r', encoding='utf8') as f:
         s = f.read()
         s = s.replace('&module', module)  # 模块名
@@ -159,26 +159,24 @@ def module_report(v,module,summery_module,path):
         s = s.replace('&pass', str(module_info['Pass']))  # 通过数
         s = s.replace('&fail', str(module_info['Fail']))  # 失败数
         s = s.replace('&skip', str(module_info['Skip']))  # 跳过数
-        sql=f'select * from test_result where version="{v}" and module="{module}"'
-        res=cd.query_all(sql)
-        detail=''
-        for i,j in enumerate(res):
-            tmp=f'''<tr><td class="bottom">{i+1}</td>
+        sql = f'select * from test_result where version="{v}" and module ="{module}"'
+        res = cd.query_all(sql)
+        detail = ''
+        for i, j in enumerate(res):
+            tmp = f'''<tr><td class="bottom">{i + 1}</td>
+                <td class="case">{j[1]}</td>
+                <td class="bottom">{j[0]}</td>
                 <td class="case">{j[2]}</td>
-                <td class="bottom">{j[1]}</td>
-                <td class="case">{j[3]}</td>
-                <td class="bottom">{j[4]}</td>
+                <td class="bottom">{j[3]}</td>
             </tr>'''
-            detail+=tmp
-        s=s.replace('&detail',detail)
-    with open(path+f'/{module}.html','w',encoding='utf8') as f:
+            detail += tmp
+        s = s.replace('&detail', detail)
+    with open(path + f'/{module}.html', 'w', encoding='utf8') as f:
         f.write(s)
 
 
 if __name__ == '__main__':
-    total,summery_all,summery_module=get_result('V1.1')
+    total, summery_all, summery_module = get_result('V1.1')
     rpath = report_path + '/V1.1'
     make_dir(rpath)
-    r=module_report('V1.1','login',summery_module,rpath)
-
-
+    r = module_report('V1.1', 'login', summery_module, rpath)
